94fd9cc278d1271a4f43323e66411a0eb3629074
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-21 10:35:53 -0700

    implement shake gesture to reset tip calculation

--- TippCalc/ViewController.m
+++ TippCalc/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+    
     self.billAmount = 0;
     self.tipAmount = 15;
     self.numberOfPersons = 2;
@@ -37,6 +40,46 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)orientationChanged:(NSNotification *)notification {
+    // Respond to changes in device orientation
+}
+
+-(void) viewDidDisappear {
+    // Request to stop receiving accelerometer events and turn off accelerometer
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+}
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self becomeFirstResponder];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        // User was shaking the device. Post a notification named "shake."
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"shake" object:self];
+        self.billAmount = 0;
+        self.tipAmount = 15;
+        self.numberOfPersons = 2;
+        self.totalTipAmount = 0;
+        self.totalBillAmount = 0;
+        self.tipPerPersonAmount = 0;
+        self.billPerPersonAmount = 0;
+        self.tipLabel.text = [NSString stringWithFormat:@"%d%%", self.tipAmount];
+        self.personLabel.text = [NSString stringWithFormat:@"%d", self.numberOfPersons];
+        self.totalTipLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalTipAmount];
+        self.totalBillLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalBillAmount];
+        self.tipPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.tipPerPersonAmount];
+        self.billPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.billPerPersonAmount];
+        self.billAmountInput.text = [NSString stringWithFormat:@"0.00"];
+    }
 }
 
 - (IBAction)billAmountInput:(UITextField *)sender {
