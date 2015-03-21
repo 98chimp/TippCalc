94ff2fa80270665974e65921e48b5cb001ba1c86
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-20 20:02:55 -0700

    Basic and stretch goals implemented

--- TippCalc/ViewController.h
+++ TippCalc/ViewController.h
@@ -8,8 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
 
+@property (strong, nonatomic) IBOutlet UITextField *billAmountInput;
+@property (strong, nonatomic) IBOutlet UILabel *tipLabel;
+@property (strong, nonatomic) IBOutlet UISlider *tipSliderInput;
+@property (strong, nonatomic) IBOutlet UILabel *personLabel;
+@property (strong, nonatomic) IBOutlet UISlider *personSliderInput;
+@property (strong, nonatomic) IBOutlet UIButton *tipCalculatorLabel;
+@property (strong, nonatomic) IBOutlet UILabel *totalTipLabel;
+@property (strong, nonatomic) IBOutlet UILabel *totalBillLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tipPerPersonLabel;
+@property (strong, nonatomic) IBOutlet UILabel *billPerPersonLabel;
+
+@property (assign, nonatomic) int tipAmount;
+@property (assign, nonatomic) double billAmount;
+@property (assign, nonatomic) int numberOfPersons;
+@property (assign, nonatomic) double totalTipAmount;
+@property (assign, nonatomic) double totalBillAmount;
+@property (assign, nonatomic) double tipPerPersonAmount;
+@property (assign, nonatomic) double billPerPersonAmount;
+
+- (IBAction)billAmountInput:(id)sender;
+- (IBAction)tipSlider:(id)sender;
+- (IBAction)personSlider:(id)sender;
+- (IBAction)tipCalculatorButton:(id)sender;
 
 @end
 
--- TippCalc/ViewController.m
+++ TippCalc/ViewController.m
@@ -17,11 +17,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.billAmount = 0;
+    self.tipAmount = 15;
+    self.numberOfPersons = 2;
+    self.totalTipAmount = 0;
+    self.totalBillAmount = 0;
+    self.tipPerPersonAmount = 0;
+    self.billPerPersonAmount = 0;
+    self.tipLabel.text = [NSString stringWithFormat:@"%d%%", self.tipAmount];
+    self.personLabel.text = [NSString stringWithFormat:@"%d", self.numberOfPersons];
+    self.totalTipLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalTipAmount];
+    self.totalBillLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalBillAmount];
+    self.tipPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.tipPerPersonAmount];
+    self.billPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.billPerPersonAmount];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)billAmountInput:(UITextField *)sender {
+    self.billAmount = [sender.text floatValue];
+    [self recalculateTip];
+}
+
+- (void)recalculateTip {
+    self.totalTipAmount = self.billAmount * self.tipAmount / 100;
+    self.totalTipLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalTipAmount];
+    self.totalBillAmount = self.billAmount + self.totalTipAmount;
+    self.totalBillLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalBillAmount];
+    
+    self.tipPerPersonAmount = self.totalTipAmount / self.numberOfPersons;
+    self.tipPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.tipPerPersonAmount];
+    self.billPerPersonAmount = self.totalBillAmount / self.numberOfPersons;
+    self.billPerPersonLabel.text = [NSString stringWithFormat:@"$%.2f", self.billPerPersonAmount];
+}
+
+- (IBAction)tipSlider:(id)sender {
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"%d%%", self.tipAmount];
+    self.tipAmount = self.tipSliderInput.value;
+    [self recalculateTip];
+}
+
+- (IBAction)personSlider:(id)sender {
+    self.personLabel.text = [NSString stringWithFormat:@"%d", self.numberOfPersons];
+    self.numberOfPersons = self.personSliderInput.value;
+    [self recalculateTip];
+}
+
+- (IBAction)tipCalculatorButton:(id)sender {
+    [self recalculateTip];
+    [self.view endEditing:YES];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touchesBegan:withEvent:");
+    [self.view endEditing:YES];
+    [super touchesBegan:touches withEvent:event];
 }
 
 @end
